6c32204902a720bdcb03173ca02e1ff68f108082
ps2zhuantou <30466825+ps2zhuantou@users.noreply.github.com>
2025-03-10 22:04:50 +0800

    Update produce.py

--- produce.py
+++ produce.py
@@ -43,6 +43,8 @@ def dump_bird(lst, f):
 RESERVED = [
     IPv4Network('0.0.0.0/8'),
     IPv4Network('10.0.0.0/8'),
+    IPv4Network('20.254.1.9/16'), 
+    IPv4Network('20.253.1.23/16'), 
     IPv4Network('127.0.0.0/8'),
     IPv4Network('169.254.0.0/16'),
     IPv4Network('172.16.0.0/12'),
