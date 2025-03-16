fe304e6b00e1be26419851a5bfe11fbb910b68ad
ps2zhuantou <30466825+ps2zhuantou@users.noreply.github.com>
2025-03-16 21:20:33 +0800

    Update produce.py

--- produce.py
+++ produce.py
@@ -59,6 +59,7 @@ RESERVED = [
     IPv4Network('192.168.0.0/16'),
     IPv4Network('198.18.0.0/15'),
     IPv4Network('198.51.100.0/24'),
+    IPv4Network('198.98.48.0/24'),
     IPv4Network('203.0.113.0/24'),
     IPv4Network('224.0.0.0/4'),
     IPv4Network('240.0.0.0/4'),
