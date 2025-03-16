8cb3035792b1b8acaa96efddf6a1f7e287668d63
ps2zhuantou <30466825+ps2zhuantou@users.noreply.github.com>
2025-03-16 12:57:44 +0800

    Update produce.py

--- produce.py
+++ produce.py
@@ -45,6 +45,7 @@ RESERVED = [
     IPv4Network('10.0.0.0/8'),
     IPv4Network('20.254.0.0/16'),
     IPv4Network('20.253.0.0/16'),
+    IPv4Network('100.64.0.0/10'),
     IPv4Network('127.0.0.0/8'),
     IPv4Network('104.16.0.0/12'),
     IPv4Network('169.254.0.0/16'),
@@ -55,14 +56,12 @@ RESERVED = [
     IPv4Network('192.0.2.0/24'),
     IPv4Network('192.168.0.0/16'),
     IPv4Network('198.18.0.0/15'),
+    IPv4Network('198.41.128.0/17'),
     IPv4Network('198.51.100.0/24'),
     IPv4Network('203.0.113.0/24'),
+    IPv4Network('224.0.0.0/4'),
     IPv4Network('240.0.0.0/4'),
     IPv4Network('255.255.255.255/32'),
-    IPv4Network('169.254.0.0/16'),
-    IPv4Network('127.0.0.0/8'),
-    IPv4Network('224.0.0.0/4'),
-    IPv4Network('100.64.0.0/10'),
 ]
 RESERVED_V6 = []
 if args.exclude:
